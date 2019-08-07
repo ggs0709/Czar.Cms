6467375c1e57766d6020325c31c4832af93b4fdb
ggs0709 <ggs0709@163.com>
2019-08-07 16:37:26 +0800

    新增第三篇文章的示例代码

---
+++ Sample02/WebApplication3/Controllers/ContentController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class ContentController : Controller
+    {
+        private readonly Content contents;
+        public ContentController(IOptions<Content> option)
+        {
+            contents = option.Value;
+        }
+        /// <summary>
+        /// 首页显示
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Index()
+        {
+
+            return View(new ContentViewModel { Contents = new List<Content> { contents } });
+        }
+
+        ///// <summary>
+        ///// 首页显示
+        ///// </summary>
+        ///// <returns></returns>
+        //public IActionResult Index()
+        //{
+        //    var contents = new List<Content>();
+        //    for (int i = 1; i < 11; i++)
+        //    {
+        //        contents.Add(new Content { Id = i, title = $"{i}的标题", content = $"{i}的内容", status = 1, add_time = DateTime.Now.AddDays(-i) });
+        //    }
+        //    return View(new ContentViewModel { Contents = contents });
+        //}
+    }
+}
